ec7f27a97f87f1d8953818cfc37d8490f8207d4e
smjavac <sm.yusupov@mail.ru>
2019-07-08 10:27:21 +0300

    notebook

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -8,12 +8,20 @@ import ru.said.service.UserService;
 import static org.mockito.Mockito.*;
 
 import java.sql.*;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import ru.said.bean.User;
+
+import static org.mockito.Mockito.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class UserServiceTest {
     @Test
-    public void testGetAll() throws SQLException {
+    public void testGetall () throws SQLException {
         Connection connection = mock(Connection.class); //создаем mock для Connection
         Statement statement = mock(Statement.class); //создаем mock для Statement
         ResultSet resultSet = mock(ResultSet.class); //создаем mock для ResultSet
@@ -21,19 +29,40 @@ public class UserServiceTest {
         when(connection.createStatement()).thenReturn(statement); //связываем connection и statement
         String sql = "SELECT * FROM  ddt_users";
         when(statement.executeQuery(sql)).thenReturn(resultSet); //связываем  statement и resultSet
-        when(resultSet.next()).thenReturn(false);//задаем поведение для resultSet
         when(resultSet.next()).thenReturn(true, false);
-        List<User> userList = UserService.getAll(connection);
-        List<User> userList3 = UserService.getAll(connection);
-        List<User> userList1 = new ArrayList<User>();
-        List<User> userList2 = new ArrayList<User>();
-        userList2.add(new User());
 
-        Assert.assertEquals(userList2.size(), userList.size());
-        Assert.assertEquals(userList1, userList3);
-      //  verify(resultSet).next();//проверяем, что вызывался метод next() у resulSet
+        List<User> userListFromUserService = UserService.getAll(connection);
+        List<User> userList1 = new ArrayList<User>();
+        userList1.add(new User());
+
+        //   Assert.assertEquals(userList1, userListFromUserService);
+        Assert.assertEquals(userList1.size(), userListFromUserService.size());
+
+        verify(resultSet, atLeastOnce()).next();//проверяем, что вызывался метод next() у resulSet
         verify(resultSet).close();//проверяем, что resultSet закрыли после использования
         verify(statement).close();//проверяем, что statement закрыли после использования
+    }
+
+
+    @Test
+    public void test2Getall () throws SQLException {
+        Connection connection = mock(Connection.class);
+        Statement statement = mock(Statement.class);
+        ResultSet resultSet = mock(ResultSet.class);
+
+        when(connection.createStatement()).thenReturn(statement);
+        String sql = "SELECT * FROM  ddt_users";
+        when(statement.executeQuery(sql)).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(false);
+
+        List<User> userListFromUserService = UserService.getAll(connection);
+        List<User> userList = new ArrayList<User>();
+
+        Assert.assertEquals(userList, userListFromUserService);
+
+        verify(resultSet).next();
+        verify(resultSet).close();
+        verify(statement).close();
     }
 
 
