6205ed4c2219ba715ee19f7bbd07fe31ce1021b3
smjavac <sm.yusupov@mail.ru>
2019-07-09 23:27:07 +0300

    notebook

--- src/test/java/ru/said/service/UserServiceTest.java
+++ src/test/java/ru/said/service/UserServiceTest.java
@@ -13,30 +13,9 @@ import java.util.List;
 
 
 public class UserServiceTest {
+
     @Test
     public void test1GetAll() throws SQLException {
-        Connection connection = mock(Connection.class); //создаем mock для Connection
-        Statement statement = mock(Statement.class); //создаем mock для Statement
-        ResultSet resultSet = mock(ResultSet.class); //создаем mock для ResultSet
-
-        when(connection.createStatement()).thenReturn(statement); //связываем connection и statement
-        String sql = "SELECT * FROM  ddt_users";
-        when(statement.executeQuery(sql)).thenReturn(resultSet); //связываем  statement и resultSet
-        when(resultSet.next()).thenReturn(true, false);
-
-        List<User> userListFromUserService = UserService.getAll(connection);
-
-        Assert.assertEquals(1, userListFromUserService.size());
-
-
-        verify(resultSet, atLeastOnce()).next();//проверяем, что вызывался метод next() у resulSet
-        verify(resultSet).close();//проверяем, что resultSet закрыли после использования
-        verify(statement).close();//проверяем, что statement закрыли после использования
-    }
-
-
-    @Test
-    public void test2GetAll() throws SQLException {
         Connection connection = mock(Connection.class);
         Statement statement = mock(Statement.class);
         ResultSet resultSet = mock(ResultSet.class);
@@ -47,7 +26,6 @@ public class UserServiceTest {
         when(resultSet.next()).thenReturn(false);
 
         List<User> userListFromUserService = UserService.getAll(connection);
-        List<User> userList = new ArrayList<User>();
 
         // Assert.assertEquals(userList, userListFromUserService);
         Assert.assertTrue(userListFromUserService.isEmpty());
@@ -57,7 +35,7 @@ public class UserServiceTest {
     }
 
     @Test
-    public void test3GetAll() throws SQLException {
+    public void test2GetAll() throws SQLException {
         Connection connection = mock(Connection.class);
         Statement statement = mock(Statement.class);
         ResultSet resultSet = mock(ResultSet.class);
@@ -72,7 +50,7 @@ public class UserServiceTest {
 
         List<User> userListFromUserService = UserService.getAll(connection);
         User user = userListFromUserService.get(0);
-
+        Assert.assertEquals(1, userListFromUserService.size());
         Assert.assertEquals("said", user.getUserName());
         Assert.assertEquals("user", user.getLogin());
         Assert.assertEquals("12345678", user.getPassword());
